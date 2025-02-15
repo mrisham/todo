7c5c573d7e11d8ed8239ef9c295245f76fe606da
Your Name <you@example.com>
2025-02-15 17:55:38 +0530

    completed todo task

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
+import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  const arr = [
-    { id: 1, name: "ms" },
-    { id: 2, name: "sm" },
-  ];
+  const [todo, setTodo] = useState("");
+  const [allTodo, setAllTodo] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("todo", todo);
+    console.log("aatodo", allTodo);
+    if (todo != "")
+      setAllTodo([{ id: `${todo}-${Date.now()} `, todo }, ...allTodo]);
+  };
+
   return (
     <div className="app">
       <div className="container">
         <h1>Todo List App</h1>
-        <form className="todoForm">
-          <input type="text" />
-          <button>Add</button>
+        <form className="todoForm" onSubmit={handleSubmit}>
+          <input type="text" onChange={(e) => setTodo(e.target.value)} />
+          <button type="submit">Add</button>
         </form>
         <ul className="all-todos">
-          <li className="single-todo">
-            <span className="todo-text">LEarn react</span>
-            <button>edit</button>
-            <button>delete</button>
-          </li>
+          {allTodo.map((t) => (
+            <li className="single-todo">
+              <span className="todo-text">{t.todo}</span>
+              <button>edit</button>
+              <button>delete</button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
